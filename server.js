e476cfce5f4ddb24cb8c0d490410dcc0c000528d
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-03-01 14:49:20 -0700

    Use jade to render views
    - Add welcome message to index.jade

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-// app.locals.title = 'Crowdsource';
+app.set('view engine', 'jade');
+app.locals.title = 'Crowdsource';
 
 app.get('/', (request, response) => {
-  response.send('Hi');
+  response.render('index');
 });
 
 app.listen(app.get('port'), () => {
-  console.log(`Application is running on ${app.get('port')}`);
+  console.log(`${app.locals.title} is running on ${app.get('port')}`);
 });
 
 module.exports = app;
