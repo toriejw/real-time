220d211ce53fddf6d2120d7428355632d0f2d03b
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-03-01 14:25:43 -0700

    Set up basic server test

---
+++ test/server-test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const app = require('../server');
+const request = require('request');
+
+describe('Server', () => {
+
+  before(done => {
+    this.port = 9876;
+
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done();
+    });
+
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876'
+    });
+  });
+
+  after(() => {
+    this.server.close();
+  });
+
+  describe('GET /', () => {
+    it('serves the home page with status 200', (done) => {
+      this.request.get('/', (error, response) => {
+        assert.equal(response.statusCode, 200);
+
+        done();
+      });
+    });
+  });
+
+});
