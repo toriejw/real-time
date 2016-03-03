57bdd9ae4ae9b9ab410bca25ff7e511a687f3470
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-03-02 18:48:21 -0700

    Create connection between server and client
    - When user creates poll, it's stored on the server and sends
    back urls to the client

--- public/scripts/create-poll.js
+++ public/scripts/create-poll.js
@@ -1,3 +1,5 @@
+var socket = io();
+
 var addResponseButton = document.getElementById('add-response');
 
 addResponseButton.addEventListener('click', function () {
@@ -7,17 +9,43 @@ addResponseButton.addEventListener('click', function () {
 var createPollButton = document.getElementById('create-poll');
 
 createPollButton.addEventListener('click', function () {
-  console.log('create poll plz')
+  var poll = getPollInfo();
+
+  socket.send('pollCreated', poll);
+});
+
+socket.on('pollSuccessfullyCreated', function (urls) {
+  console.log(urls);
 });
 
 function addAdditionalResponseOption() {
-  var formResponses = document.getElementById('form-responses');
+  var formResponses = getResponsesDiv();
 
   var additionalResponse = document.createElement('INPUT');
-  additionalResponse.className = 'form-control';
-  
+  additionalResponse.className = 'form-control poll-response';
+
   var lineBreak = document.createElement('BR');
 
   formResponses.appendChild(additionalResponse);
   formResponses.appendChild(lineBreak);
+}
+
+function getPollInfo() {
+  return { question: getPollQuestion(), responses: getPollResponses() };
+}
+
+function getPollQuestion() {
+  return document.getElementById('poll-question').value;
+}
+
+function getPollResponses() {
+  var formResponses = getResponsesDiv().getElementsByClassName('poll-response');
+
+  return Array.prototype.map.call(formResponses, function(response) {
+    return response.value;
+  });
+}
+
+function getResponsesDiv() {
+  return document.getElementById('form-responses');
 }
--- server.js
+++ server.js
@@ -21,8 +21,25 @@ server.listen(port, () => {
   console.log(`${app.locals.title} is listening on port ${app.get('port')}`);
 });
 
+var polls = {};
+
 const socketIo = require('socket.io');
 const io = socketIo(server);
 
+io.on('connection', function (socket) {
+
+  socket.on('message', function (channel, poll) {
+    if (channel === 'pollCreated') {
+      polls[socket.id] = poll;
+
+      var baseUrl = socket.conn.request.headers.host;
+
+      socket.emit('pollSuccessfullyCreated', {pollUrl: baseUrl + socket.id,
+                                              adminUrl: baseUrl + '/admin' + socket.id});
+      // send back to client the url of the poll and the admin url for the poll (user must refresh page to make new poll)
+    }
+  });
+
+});
 
 module.exports = app;
