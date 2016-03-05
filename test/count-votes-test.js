dfe41f90bfcca67aa8e3ffefca5964ddd897065b
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-03-05 15:51:06 -0700

    Add test for countVotes

--- test/count-votes-test.js
+++ test/count-votes-test.js
@@ -2,9 +2,20 @@ const countVotes = require('../lib/count-votes.js');
 const assert = require('assert');
 
 describe ('countVotes', () => {
+  var votes = {'socket-id1': 'first',
+               'socket-id2': 'first',
+               'socket-id3': 'second',
+               'socket-id4': 'first'};
+
+  var responses = ['first', 'second', 'third'];
 
   it('counts the votes for a given list of choices', function () {
-    assert(true);
+    actualVoteCount = countVotes(votes, responses);
+    expectedVoteCount = { 'first': 3, 'second': 1, 'third': 0 };
+
+    assert.equal(actualVoteCount.first, expectedVoteCount.first);
+    assert.equal(actualVoteCount.second, expectedVoteCount.second);
+    assert.equal(actualVoteCount.third, expectedVoteCount.third);
   });
 
 });
