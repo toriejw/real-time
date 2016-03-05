8fca1b2ba6b132b4fb3eb129eb017245c60f6212
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-03-05 15:44:34 -0700

    Fix type in close-poll.js

--- public/scripts/close-poll.js
+++ public/scripts/close-poll.js
@@ -27,7 +27,7 @@ setPollCloseTimeButton.addEventListener('click', function () {
   var hour = document.getElementById('close-poll-hour');
   var minute = document.getElementById('close-poll-minute');
 
-  document.getElementById('poll-schedule-status').innerText = 'Your poll is schedule to close at ' + hour.value + ':' + minute.value + ' UTC time.';
+  document.getElementById('poll-schedule-status').innerText = 'Your poll is scheduled to close at ' + hour.value + ':' + minute.value + ' UTC time.';
 
   socket.send('setPollCloseTime', { hour: hour, minute: minute, pollId: poll.id});
 });
---
+++ test/count-votes-test.js
@@ -0,0 +1,10 @@
+const countVotes = require('../lib/count-votes.js');
+const assert = require('assert');
+
+describe ('countVotes', () => {
+
+  it('counts the votes for a given list of choices', function () {
+    assert(true);
+  });
+
+});
